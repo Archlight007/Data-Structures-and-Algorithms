7bed312e1d7ba69b6bc786d8c1422c3471b322c4
Nimesh <nimeshbisht2000@gmail.com>
2021-08-14 23:59:49 +0530

    Max Min array another

---
+++ C++ basics/maxminarray2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int* arr = new int[n];
+    for (int i=0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    int maxNo=INT_MIN;
+    int minNo=INT_MAX;
+    for (int i=0 ; i<n ; i++){
+        maxNo=max(maxNo,arr[i]);
+        minNo=min(minNo,arr[i]);
+    }
+    cout<<maxNo<<" "<<minNo<<endl;
+    return 0;
+    delete [] arr;
+}

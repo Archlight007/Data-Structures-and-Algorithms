4399282ecf038dffaa33b86350c795acbcd1abac
Nimesh <nimeshbisht2000@gmail.com>
2021-08-16 01:37:31 +0530

    Binary Search in Arrays

---
+++ Arrays/BinarySearch.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[] , int n , int key){
+    int s=0;
+    int e=n;
+    while(s<=e){
+        int mid=(s+e)/2;
+
+        if(arr[mid]==key){
+            return mid;
+        } else if(arr[mid]>key){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    
+    int n;
+    cin>>n;
+    
+    int* arr = new int[n];
+    for (int i=0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    cout<<binarySearch(arr,n,key)<<endl;
+    delete [] arr;
+    return 0;
+}

675b1d840d593973fc1e263b0c981b8e6b192b41
Nimesh <nimeshbisht2000@gmail.com>
2021-11-14 14:46:08 +0530

    FizzBuzz using vectors

--- Arrays/vector.cpp
+++ Arrays/vector.cpp
@@ -5,8 +5,16 @@ int main(){
     // Vector Syntax
     vector<int> arr={1,2,3,4,5,6};
 
+    // Command to remove last element 
+    arr.pop_back(); 
+
     //Push-back function(it inserts an element in the last of array) 0(1)
     arr.push_back(7);
+
+    // Print all elements of vector
+    for (int i=0 ; i<arr.size() ; i++){
+        cout<<arr[i]<<endl;
+    }
 
     // Size of vector(Number of elements)
     cout<<arr.size()<<endl;
